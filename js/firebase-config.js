a06e14db942a10d8a889faada12430d1d647dd38
Noodle125 <153040693+Noodle125@users.noreply.github.com>
2024-05-18 10:08:02 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAlLRg-irrAb1RNC-zvWWkb79GWqLe0XDc",
-    authDomain: "stx-373b2.firebaseapp.com",
-    databaseURL: "https://stx-373b2-default-rtdb.firebaseio.com",
-    projectId: "stx-373b2",
-    storageBucket: "stx-373b2.appspot.com",
-    messagingSenderId: "714624842537",
-    appId: "1:714624842537:web:54fc08b0fdbd6226e1d538"
+  apiKey: "AIzaSyADmNV47sId0qdGDiWvl6awe1_5PSRncjM",
+  authDomain: "ndx-b1e1d.firebaseapp.com",
+  databaseURL: "https://ndx-b1e1d-default-rtdb.firebaseio.com",
+  projectId: "ndx-b1e1d",
+  storageBucket: "ndx-b1e1d.appspot.com",
+  messagingSenderId: "512322480998",
+  appId: "1:512322480998:web:d8d8948413e254f330f6a7"
 };
 
 // Initialize Firebase
